6c972f76eca3e3b44991e9cb7740ac20c0394fa2
mahdi-barzegar-nazari <m.barzegar.nazari@gmail.com>
2026-09-16 20:58:12 +0330

    feat: add dictionary and tuple basics with practice exercises

---
+++ basics/10_dict_practice.py
@@ -0,0 +1,12 @@
+fruit_prices = {
+    "apple": 1500,
+    "banana": 1000,
+    "orange": 1200
+}
+
+fruit_prices["banana"] = 1100
+
+fruit_prices.pop("apple")
+#del fruit_prices['apple']
+
+print(fruit_prices)
---
+++ basics/12_tuple_practice.py
@@ -0,0 +1,9 @@
+coordinates = (3, 4)
+
+x, y = coordinates
+#x = coordinates[0]
+#y = coordinates[1]
+
+distance = ((x**2) + (y**2))**0.5
+
+print(distance)
